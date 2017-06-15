6522c4be882302edf3ef50a367820a1325a55258
David Heidrich <me@bowlingx.com>
2017-06-15 11:03:37 +0200

    adjusted filters

--- app/Filters.scala
+++ app/Filters.scala
@@ -1,6 +1,7 @@
 import javax.inject.Inject
 
 import play.api.http.DefaultHttpFilters
+import play.filters.cors.CORSFilter
 import play.filters.csrf.CSRFFilter
 import play.filters.headers.SecurityHeadersFilter
 import play.filters.hosts.AllowedHostsFilter
@@ -15,9 +16,11 @@ import play.filters.hosts.AllowedHostsFilter
 class Filters @Inject()(
                          csrfFilter: CSRFFilter,
                          allowedHostsFilter: AllowedHostsFilter,
-                         securityHeadersFilter: SecurityHeadersFilter
+                         securityHeadersFilter: SecurityHeadersFilter,
+                         corsFilter: CORSFilter
                        ) extends DefaultHttpFilters(
   csrfFilter,
   allowedHostsFilter,
-  securityHeadersFilter
+  securityHeadersFilter,
+  corsFilter
 )
